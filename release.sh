dcb59eda2f635ee43a2dd1b0020d2794ee0d0305
zhangdezheng <2549626703@qq.com>
2024-05-25 16:52:03 +0800

    feat: update release.sh

--- release.sh
+++ release.sh
@@ -10,22 +10,13 @@ if [ -z "$1" ];
 then
 VERSION=`npx select-version-cli` # get version from prompt
 elif [[ "$OSTYPE" == "darwin"* ]]; 
-then  
-    # echo "This is macOS"  
+then
 VERSION=`npm run get:version:mac --silent` # get current version from package.json
 echo "Override version with 'v$VERSION'."
-
-# elif [[ "$OSTYPE" == "msys"* ]]; then  
-    # echo "This is likely Windows using MSYS2"  
-else [[ "$OSTYPE" == "win32"* ]];
-then  
-# echo "This is likely Windows using Cygwin"
+elif [[ "$OSTYPE" == "win32"* ]];
+then
 VERSION=`npm run get:version:win --silent` # get current version from package.json
 echo "Override version with 'v$VERSION'."
-
-# else
-# VERSION=`npm run get:version --silent` # get current version from package.json
-# echo "Override version with 'v$VERSION'."
 fi
 
 read -p "Please enter release description: " DESCRIPTION
@@ -39,6 +30,8 @@ npm run build
 version=$VERSION desc=$DESCRIPTION npm run upload
 
 # bump version
+echo "Releasing $VERSION ..."
+
 npm version $VERSION --message "refactor: 版本 v$VERSION 发布" --allow-same-version
 
 # push
